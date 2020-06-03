0b79b5cf1fefed10de70a97d07f13b27f4c8e827
Daniel Bugge <danielbugge96@gmail.com>
2020-06-03 22:42:56 +0100

    Add tests for use cases

---
+++ app/tests/test_use_cases.py
@@ -0,0 +1,10 @@
+# from app.sample.use_cases import vehicles_repo
+#
+# def test_get_vehicles(mocker):
+#     owner = 'owner'
+#     mocker.patch.object(vehicles_repo, 'get_vehicles')
+#     vehicles_repo.get_vehicles.return_value = 120
+#     vehicles_repo.get_vehicles.asser
+#
+# def test_get_user():
+#     assert 1 == 1
