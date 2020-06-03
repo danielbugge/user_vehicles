4a0aea36608561c5b736d681479611624657e3e6
Daniel Bugge <danielbugge96@gmail.com>
2020-06-03 22:42:27 +0100

    Add REST logic

--- app/main.py
+++ app/main.py
@@ -1,17 +1,27 @@
 import uvicorn
-
-from fastapi import FastAPI
-from starlette.responses import RedirectResponse
-
+from fastapi import Depends, FastAPI
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from app.sample.entities.token import Token
+from app.sample.entities.user import User
+import app.sample.exceptions as exceptions
+import app.sample.authentication as authentication
+import app.sample.use_cases as use_cases
 
 app = FastAPI()
+security = HTTPBasic()
 
 
-@app.get("/", include_in_schema=False)
-async def read_root():
-    return RedirectResponse(url='/docs')
+@app.post("/token", response_model=Token)
+def login_for_access_token(credentials: HTTPBasicCredentials = Depends(security)):
+    user = use_cases.get_user(credentials.username, credentials.password)
+    if not user:
+        raise exceptions.unauthenticated_user
+    access_token = authentication.generate_access_token(credentials.username)
+    return {"access_token": access_token, "token_type": "bearer"}
 
+@app.get("/vehicles")
+def get_own_vehicles(user: User = Depends(authentication.get_current_active_user)):
+    return use_cases.get_vehicles(user.username)
 
 if __name__ == '__main__':
-    # only debug
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=80)
